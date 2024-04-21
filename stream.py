b32f0e3298fd1ffe8f7b99fa116c8050af00548a
jaffer-ali <alijaffer3000@gmail.com>
2024-04-21 18:37:37 -0400

    scaling

--- stream.py
+++ stream.py
@@ -13,9 +13,17 @@ class grbl_write():
         self.s.flushInput()  # Flush startup text in serial input
         self.cond = True
     
+    def scale(self, xpos, ypos, scale):
+        self.x_new = (float(xpos)-3.27)*scale
+        self.y_new = (float(ypos)-1.36)*scale
+
+        return str(self.x_new), str(self.y_new)
+
     def grbl_stream(self,xpos,ypos):
+        xpos, ypos = self.scale(xpos, ypos, -100)
+
         if self.cond is True:
-            line = '$X\nG92 X0 Y0 Z0\nG17 G21 G90 G94 G54\nG01 X0 Y0 Z0 F5000\n'
+            line = '$X\nG92 X0 Y0 Z0\nG17 G21 G90 G94 G54\nG01 X0 Y0 Z0 F6000\n'
             self.cond = False
         else:
             line = 'X{} Y{}'.format(xpos,ypos) #input x,y position from arduino
@@ -33,17 +41,17 @@ class hapkit_read():
     
     def hapkit_stream(self):
         xypos = self.s2.readline().decode().strip()
-        xpos = xypos.split('a')[0]
-        ypos = xypos.split('a')[1]
+        xpos = xypos.split('a')[1]
+        ypos = xypos.split('a')[0]
         return xpos, ypos
 
       
 
-#grbl = grbl_write('COM3')    
-hapkit = hapkit_read('COM5')
+grbl = grbl_write('COM10')    
+hapkit = hapkit_read('COM8')
 
 while True:
-    #grbl.grbl_stream(xpos,ypos)
+    grbl.grbl_stream(xpos,ypos)
     xpos,ypos = hapkit.hapkit_stream()
     print(xpos,ypos)
     
